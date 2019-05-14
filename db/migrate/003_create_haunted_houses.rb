fc5c1378a6ad07e1d0234afb4a0230bad6c60800
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-14 02:03:36 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,14 +3,14 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.string :name
-      # t.string :location
-      # t.string :theme
-      # t.integer :price
-      # t.boolean :family
-      # t.datetime :opening_time
-      # t.datetime :closing_time
-      # t.text :long_description
-      # t.timestamps null: false
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.text :long_description
+      t.timestamps null: false
     end
   end
 end
